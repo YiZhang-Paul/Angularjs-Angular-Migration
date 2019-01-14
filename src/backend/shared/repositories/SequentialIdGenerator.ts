6a761083b4db44eb26efc7ba8b375dcdb98b504e
YI ZHANG <scaccterran@gmail.com>
2019-01-13 23:27:08 -0500

    removed redundant constructor

--- src/backend/shared/repositories/SequentialIdGenerator.ts
+++ src/backend/shared/repositories/SequentialIdGenerator.ts
@@ -1,13 +1,6 @@
-import { Document, Model } from 'mongoose';
-
 import IdGenerator from './IdGenerator';
 
 export default class SequentialIdGenerator extends IdGenerator {
-
-    constructor(model: Model<Document, {}>) {
-
-        super(model);
-    }
 
     public showNext(id: string): string {
 
