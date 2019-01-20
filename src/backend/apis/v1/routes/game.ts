eb58cfcdc671e64181a6de6f38a0c16d57bcb4d0
YI ZHANG <scaccterran@gmail.com>
2019-01-20 01:05:22 -0500

    refactored game router

--- src/backend/apis/v1/routes/game.ts
+++ src/backend/apis/v1/routes/game.ts
@@ -1,78 +1,43 @@
-import http from 'axios';
 import { Request, Response, Router } from 'express';
 
-import GameDataAdapter from '../../../data-collector/services/GameDataAdapter';
-import GameDataReducer from '../../../data-collector/services/GameDataReducer';
+import gameDataCollectorPromise from '../../../data-collector/factories/GameDataCollectorFactory';
 import GameDataStorageManager from '../../../data-collector/services/GameDataStorageManager';
 import GameRepositoryFactory from '../../../shared/repositories/GameRepositoryFactory';
 import MemoryDataStore from '../../../data-collector/services/MemoryDataStore';
 import MongoDbGameDataStore from '../../../data-collector/services/MongoDbGameDataStore';
-import ProviderRepositoryFactory from '../../../shared/repositories/ProviderRepositoryFactory';
 
 const router = Router();
 const gameRepository = new GameRepositoryFactory().createRepository();
-const providerRepository = new ProviderRepositoryFactory().createRepository();
 const memoryStore = new MemoryDataStore();
 const persistentStore = new MongoDbGameDataStore(gameRepository);
 const storageManager = new GameDataStorageManager(memoryStore, persistentStore);
-const adapter = new GameDataAdapter();
-const reducer = new GameDataReducer(adapter);
 
 router.get('/', async (_: Request, res: Response) => {
-
+    // TODO: retrieve from game data collector instead
     res.status(200).send(await storageManager.getFromMemory());
 });
-
+// TODO: wrap in service
 router.get('/:id', async (req: Request, res: Response) => {
 
-    const id = req.params['id'];
+    const id = +req.params['id'];
     const games = await storageManager.getFromMemory();
-    const game = games.find(_ => `${_['id']}` === id);
+    let result = games.find(_ => _['id'] === id);
 
-    if (game) {
+    if (result) {
 
-        return res.status(200).send(game);
+        return res.status(200).send([result]);
     }
 
-    const result = await gameRepository.findOne({ id });
+    const gameDataCollector = await gameDataCollectorPromise;
+    await gameDataCollector.collectById(id);
+    result = await storageManager.getFromMemory(`games/${id}`);
 
-    if (!result) {
+    if (!result.length) {
 
         return res.sendStatus(404);
     }
 
-    const result1: any[] = [];
-
-    for (const _ of result.toObject()['search_api_keys']) {
-
-        const provider = await providerRepository.findOne({ id: _['provider_id'] });
-
-        if (!provider) {
-
-            continue;
-        }
-
-        const api = provider.toObject()['urls']['search_game_url'];
-        const result2 = await http.get(`${api}/${_['provider_game_id']}`);
-        result2.data['provider_id'] = +provider.toObject()['id'];
-        result1.push(result2.data);
-    }
-
-    const final = reducer.reduce(result1)[0];
-    final['id'] = +result.toObject()['id'];
-    console.log(final);
-
-    const final1 = JSON.stringify(final, (key, value) => {
-
-        if (key === '_id' || key === '__v') {
-
-            return undefined;
-        }
-
-        return value;
-    });
-
-    res.status(200).send(JSON.parse(final1));
+    res.status(200).send(result);
 });
 
 router.all('/', (_: Request, res: Response) => res.sendStatus(405));
