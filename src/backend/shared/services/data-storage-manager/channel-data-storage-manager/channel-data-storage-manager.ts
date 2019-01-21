480510580d5b4b11fa3b4fbb0e251c50c30cc9e8
YI ZHANG <scaccterran@gmail.com>
2019-01-21 11:47:10 -0500

    channel data storage manager

---
+++ src/backend/shared/services/data-storage-manager/channel-data-storage-manager/channel-data-storage-manager.ts
@@ -0,0 +1,33 @@
+import IDataStorageManager from '../data-storage-manager.interface';
+import IMemoryDataStore from '../../data-store/memory-data-store/memory-data-store.interface';
+
+export default class ChannelDataStorageManager implements IDataStorageManager {
+
+    private readonly _cacheKey = 'channels';
+    private _memoryStore: IMemoryDataStore;
+
+    constructor(memoryStore: IMemoryDataStore) {
+
+        this._memoryStore = memoryStore;
+    }
+
+    public async addToPersistent(data: any[]): Promise<any[]> {
+
+        return this.addToMemory(data);
+    }
+
+    public async addToMemory(data: any[], key?: string): Promise<any[]> {
+
+        return this._memoryStore.set(data, key ? key : this._cacheKey);
+    }
+
+    public async getFromPersistent(): Promise<any[]> {
+
+        return this.getFromMemory();
+    }
+
+    public async getFromMemory(key?: string): Promise<any[]> {
+
+        return this._memoryStore.get(key ? key : this._cacheKey);
+    }
+}
