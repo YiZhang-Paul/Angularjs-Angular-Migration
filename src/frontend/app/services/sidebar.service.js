9c697bd79d288e015ba7c8b2f0a5defde42d4e25
YI ZHANG <scaccterran@gmail.com>
2019-01-26 10:33:44 -0500

    remove all histories

--- src/frontend/app/services/sidebar.service.js
+++ src/frontend/app/services/sidebar.service.js
@@ -88,4 +88,15 @@ angular.module('migration-sample-app')
             });
 
         }
+
+        $scope.clearHistories = function () {
+            $http.delete('http://127.0.0.1:4150/api/v1/user/histories/', {headers: {
+                'Authorization': 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+            }}).then(function() {
+                $scope.histories = [];
+            },
+            function(err) {
+                console.log(err);
+            });
+        }
     }]);
