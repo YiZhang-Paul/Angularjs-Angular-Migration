d52200b592ee80938d2d42590d29372c002000b7
YI ZHANG <scaccterran@gmail.com>
2019-01-26 13:38:51 -0500

    add missing game name on feature channel badges

--- src/frontend/app/directives/sideBar.js
+++ src/frontend/app/directives/sideBar.js
@@ -9,13 +9,18 @@ angular.module('migration-sample-app')
             scope.targetRoutes = ['followed', 'featured', 'histories'];
             scope.badges = new Map();
             $http.get('http://127.0.0.1:4150/api/v1/channels').then(function(data) {
-                scope.badges.set('Featured Channels', data.data.slice(0, 3));
+                data = data.data.slice(0, 3);
+                for (var i = 0; i < data.length; i++) {
+                    data[i].game_name = data[i].provider_game_name;
+                }
+                scope.badges.set('Featured Channels', data);
             },
             function(err) {
                 console.log(err);
             });
             scope.badges.set('Followed Channels', []);
             sideBarService.getHistories().then(function(data) {
+                console.log(data);
                 scope.badges.set('View History', data.slice(0, 3));
             },
             function(err) {
