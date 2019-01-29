ea8d37f1cd4866873d26bca97bf2e220e7f9947b
YI ZHANG <scaccterran@gmail.com>
2019-01-29 12:11:36 -0500

    added and configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+
+module.exports = {
+    entry: './src/frontend/public/app/app.module.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/frontend/public')
+    },
+    mode: 'production',
+    plugins: [
+        new ngAnnotatePlugin({
+            add: true
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
