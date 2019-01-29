c5d8a950b50227ed977e1a251b6f8b4ac6033752
YI ZHANG <scaccterran@gmail.com>
2019-01-28 23:07:45 -0500

    cleanup top navigation bar component

--- src/frontend/public/app/components/channel/channel-badge/channel-badge.component.js
+++ src/frontend/public/app/components/channel/channel-badge/channel-badge.component.js
@@ -4,16 +4,13 @@
 
 const app = angular.module('migration-sample-app');
 
-class ChannelBadgeComponent { }
-
 app.component('channelBadge', {
 
     bindings: {
 
         badge: '<'
     },
-    templateUrl: './app/components/channel/channel-badge/channel-badge.html',
-    controller: ChannelBadgeComponent
+    templateUrl: './app/components/channel/channel-badge/channel-badge.html'
 });
 
 })();
