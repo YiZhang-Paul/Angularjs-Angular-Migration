44ee6c057c4857a25c4b3b52dae489880855d8ee
YI ZHANG <scaccterran@gmail.com>
2019-01-30 00:01:54 -0500

    configured istanbul instrumenter to properly generate code coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -1,35 +1,82 @@
+const path = require('path');
+const entry = './src/frontend/public/specs.js';
+
 module.exports = function (config) {
     config.set({
         basePath: '',
         files: [
             './node_modules/angular/angular.js',
             './node_modules/angular-mocks/angular-mocks.js',
-            './src/frontend/public/app/specs.js'
+            entry
         ],
         preprocessors: {
-            './src/frontend/public/app/specs.js': ['webpack', 'sourcemap']
+            [entry]: ['webpack', 'sourcemap']
         },
         webpack: {
             devtool: 'inline-source-map',
-            mode: 'development'
+            mode: 'development',
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        use: {
+                            loader: 'istanbul-instrumenter-loader',
+                            options: { esModules: true }
+                        },
+                        include: path.resolve('./src/frontend/public/'),
+                        exclude: /\.?specs?\.js$/
+                    }
+                ]
+            }
         },
+        reporters: ['mocha', 'coverage-istanbul', 'remap-coverage'],
         autoWatch: false,
         singleRun: true,
         frameworks: ['mocha', 'chai', 'sinon'],
         browsers: ['Chrome'],
+        coverageIstanbulReporter: {
+            reports: ['text', 'text-summary'],
+            fixWebpackSourcePaths: true
+        },
+        mochaReporter: {
+            colors: {
+                success: 'green',
+                info: 'blue',
+                warning: 'cyan',
+                error: 'red'
+            },
+            symbols: {
+                success: '+',
+                info: '#',
+                warning: '!',
+                error: 'x'
+            }
+        },
         plugins: [
             'karma-chrome-launcher',
             'karma-mocha',
+            'karma-mocha-reporter',
             'karma-chai',
             'karma-sinon',
             'karma-webpack',
-            'karma-sourcemap-loader'
+            'karma-sourcemap-loader',
+            'karma-coverage',
+            'karma-remap-coverage',
+            'karma-coverage-istanbul-reporter'
         ],
         webpackMiddleware: {
-			noInfo: true,
-			stats: {
-				chunks: false
-			}
-		}
+            noInfo: true,
+            stats: {
+                chunks: false
+            }
+        },
+        coverageReporter: {
+            type: 'in-memory'
+        },
+        remapCoverageReporter: {
+            'text-summary': null,
+            json: './coverage/coverage.json',
+            html: './coverage/html'
+        }
     });
 };
