22097ce648da4abb85a305a57f7fe1c0f7482ff1
YI ZHANG <scaccterran@gmail.com>
2019-02-02 16:50:59 -0500

    extracted getChannels method

--- src/frontend/public/app/shared/services/channel-http.service.js
+++ src/frontend/public/app/shared/services/channel-http.service.js
@@ -4,12 +4,19 @@ export class ChannelHttpService {
         'ngInject';
         this.$http = $http;
 
-        this.api = 'http://127.0.0.1:4150/api/v1/games/:id/channels';
+        this.baseApi = 'http://127.0.0.1:4150/api/v1';
+    }
+
+    getChannels() {
+
+        const url = `${this.baseApi}/channels`;
+
+        return this.$http.get(url).then(response => response.data);
     }
 
     getChannelsByGameId(id) {
 
-        const url = this.api.replace(':id', id);
+        const url = `${this.baseApi}/games/${id}/channels`;
 
         return this.$http.get(url).then(response => response.data);
     }
