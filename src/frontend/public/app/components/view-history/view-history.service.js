4aeee5a28b787786078b70aac45e3f194ce14965
Yi Zhang <Yi.Zhang@flynncompanies.com>
2019-02-04 16:43:22 -0500

    extract view history methods to service

--- src/frontend/public/app/components/view-history/view-history.service.js
+++ src/frontend/public/app/components/view-history/view-history.service.js
@@ -6,6 +6,16 @@ export class ViewHistoryService {
         this.historyService = viewHistoryHttpService;
     }
 
+    getHistories() {
+
+        return this.historyService.getHistories().catch(error => {
+
+            console.log(error);
+
+            return [];
+        });
+    }
+
     addHistory(channel) {
 
         return this.historyService.addHistory(channel).then(() => {
@@ -13,5 +23,22 @@ export class ViewHistoryService {
             this.$rootScope.$broadcast('historyUpdated');
         })
         .catch(error => console.log(error));
+    }
+
+    deleteHistory(history) {
+
+        const id = history.id;
+
+        return this.historyService.deleteHistory(id).catch(error => {
+
+            console.log(error);
+        });
+    }
+
+    clearHistories() {
+
+        return this.historyService.deleteHistories()
+            .then(() => true)
+            .catch(() => false);
     }
 }
