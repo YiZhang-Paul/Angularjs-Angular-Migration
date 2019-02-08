6b39706977abee5d01a7f6e3e0c549ccdc87f980
Yi Zhang <Yi.Zhang@flynncompanies.com>
2019-02-07 23:49:06 -0500

    load bookmarks on bootstrap

--- src/frontend/public/app/app.component.js
+++ src/frontend/public/app/app.component.js
@@ -1,6 +1,17 @@
 import '../style.css';
 
-export class App { }
+export class App {
+
+    constructor(bookmarkService) {
+        'ngInject';
+        this.service = bookmarkService;
+    }
+
+    $onInit() {
+
+        this.service.cacheBookmarks();
+    }
+}
 
 export const AppComponent = {
 
