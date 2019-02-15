7f128524dc0663a5aac1ae9a4f14fcad48201ac6
Yi Zhang <Yi.Zhang@flynncompanies.com>
2019-02-15 14:07:04 -0500

    use testbed in user widget tests

--- src/frontend/public/app/components/login/user-widget/user-widget.component.spec.ts
+++ src/frontend/public/app/components/login/user-widget/user-widget.component.spec.ts
@@ -1,19 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { expect } from 'chai';
+
+import { SharedModule } from '../../../shared/shared.module';
 
 import { UserWidgetComponent } from './user-widget.component';
 
 context('user widget component unit test', () => {
 
+    let fixture: ComponentFixture<UserWidgetComponent>;
     let component: UserWidgetComponent;
 
     beforeEach('general test setup', () => {
 
-        component = new UserWidgetComponent();
+        TestBed.configureTestingModule({
+
+            imports: [SharedModule],
+            declarations: [UserWidgetComponent]
+        });
+
+        fixture = TestBed.createComponent(UserWidgetComponent);
+        component = fixture.componentInstance;
     });
 
     it('should resolve', () => {
 
         expect(component).is.not.null;
+        expect(component).to.be.instanceOf(UserWidgetComponent);
     });
 
     describe('menu options', () => {
