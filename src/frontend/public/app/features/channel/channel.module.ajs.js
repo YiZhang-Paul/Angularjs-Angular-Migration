9d2b47ea2259715430bfdbee1fd5d5ed6c343c92
Yi Zhang <Yi.Zhang@flynncompanies.com>
2019-02-17 15:48:46 -0500

    dedicated channel service

--- src/frontend/public/app/features/channel/channel.module.ajs.js
+++ src/frontend/public/app/features/channel/channel.module.ajs.js
@@ -2,6 +2,8 @@ import { ChannelCardComponent } from './channel-card/channel-card.component';
 import { FeaturedChannelListComponent } from './featured-channel-list/featured-channel-list.component';
 import { GameChannelListComponent } from './game-channel-list/game-channel-list.component';
 
+import { ChannelService } from './channel.service';
+
 const moduleName = 'sample-app-channel';
 
 export default moduleName;
@@ -9,4 +11,5 @@ export default moduleName;
 angular.module(moduleName, [])
     .component('channelCard', ChannelCardComponent)
     .component('featuredChannelList', FeaturedChannelListComponent)
-    .component('gameChannelList', GameChannelListComponent);
+    .component('gameChannelList', GameChannelListComponent)
+    .service('channelService', ChannelService);
