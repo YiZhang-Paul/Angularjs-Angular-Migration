6f5512d34f5e7e95132c4a3e47bc47016a48ab80
Yi Zhang <Yi.Zhang@flynncompanies.com>
2019-03-18 22:36:48 -0400

    migrated view history card component

--- src/frontend/public/app/features/features.module.ts
+++ src/frontend/public/app/features/features.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { ChannelModule } from './channel/channel.module';
 import { GameModule } from './game/game.module';
+import { ViewHistoryModule } from './view-history/view-history.module';
 
 @NgModule({
     imports: [
         BookmarkModule,
         ChannelModule,
-        GameModule
+        GameModule,
+        ViewHistoryModule
     ]
 })
 export class FeaturesModule { }
--- src/frontend/public/app/features/view-history/view-history.module.ajs.js
+++ src/frontend/public/app/features/view-history/view-history.module.ajs.js
@@ -1,4 +1,3 @@
-import { ViewHistoryCardComponent } from './view-history-card/view-history-card.component';
 import { ViewHistoryListComponent } from './view-history-list/view-history-list.component';
 
 import { ViewHistoryListService } from './view-history-list/view-history-list.service';
@@ -8,6 +7,5 @@ const moduleName = 'sample-app-view-history';
 export default moduleName;
 
 angular.module(moduleName, [])
-    .component('viewHistoryCard', ViewHistoryCardComponent)
     .component('viewHistoryList', ViewHistoryListComponent)
     .service('viewHistoryListService', ViewHistoryListService);
