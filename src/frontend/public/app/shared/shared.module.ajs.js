7a127be191ab70574fe8a851729689ea0cbc23dd
Yi Zhang <Yi.Zhang@flynncompanies.com>
2019-04-07 18:01:06 -0400

    removed filters

--- src/frontend/public/app/shared/shared.module.ajs.js
+++ src/frontend/public/app/shared/shared.module.ajs.js
@@ -1,15 +1,8 @@
 import { LoginPanelComponent } from './components/user-login/login-panel/login-panel.component';
 
-import { capitalizeFilter } from './filters/capitalize/capitalize.filter';
-import { shortViewCountFilter } from './filters/short-view-count/short-view-count.filter';
-import { uppercaseRomanNumeralsFilter } from './filters/uppercase-roman-numerals/uppercase-roman-numerals.filter';
-
 const moduleName = 'sample-app-shared';
 
 export default moduleName;
 
 angular.module(moduleName, ['ngAnimate', 'ngMaterial', 'toastr'])
-    .component('loginPanel', LoginPanelComponent)
-    .filter('capitalize', capitalizeFilter)
-    .filter('shortViewCount', shortViewCountFilter)
-    .filter('uppercaseRomanNumerals', uppercaseRomanNumeralsFilter);
+    .component('loginPanel', LoginPanelComponent);
